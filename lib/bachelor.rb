e492314f4314f23f87724ef7a01f34447673ff2d
Daniel Shimon <danielzshimon@gmail.com>
2018-04-18 00:25:35 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -39,5 +39,11 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_total = 0
+  num_of_contestants = 0
+  data[season].each do |contestant|
+    age_total += contestant["age"]
+    num_of_contestants += 1
+  end
+  (age_total / num_of_contestants)
 end
